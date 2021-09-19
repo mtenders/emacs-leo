425a9bc1e4598d60b4e5ab9b7b1d188ed947ea34
mousebot <mousebot@riseup.net>
2021-09-19 10:59:17 +0200

    leo-xml-inspect - vars for sections by POS

--- leo-inspect.el
+++ leo-inspect.el
@@ -10,13 +10,33 @@ The result is also saved to variable leo-xml-inspect-result for playing."
     (erase-buffer)
     (goto-char (point-min))
     (let ((inhibit-read-only t))
-      (print xml (current-buffer))
-      (emacs-lisp-mode)
-      (pp-buffer)
       (setq leo-xml-inspect-result xml)
       (setq leo-xml-inspect-section-list
             (car (leo--get-result-section-list
-                   (car leo-xml-inspect-result))))
+                  (car leo-xml-inspect-result))))
+      (setq leo-xml-inspect-sections-as-list
+            (leo--get-result-sections-as-list
+             (leo--get-result-section-list
+              (car leo-xml-inspect-result))))
+      ;; get sections by POS:
+      (mapcar (lambda (x)
+                (let ((pos (leo--get-section-part-of-speech x)))
+                  (cond
+                   ((equal pos "Substantive")
+                  (setq leo-xml-inspect-single-section-noun x))
+                 ((equal pos "Verben")
+                  (setq leo-xml-inspect-single-section-verb x))
+                 ((equal pos "Adjektive/Adverbien")
+                  (setq leo-xml-inspect-single-section-adj-adv x))
+                 ((equal pos "Präpositionen/Pronomen")
+                  (setq leo-xml-inspect-single-section-praep x))
+                 ((equal pos "Beispeiele")
+                  (setq leo-xml-inspect-single-section-eg x))
+                 ((equal pos "Wendungen/Ausdrücke")
+                  (setq leo-xml-inspect-single-section-wendung x))
+                 (t
+                  (setq leo-xml-inspect-single-section-other x)))))
+              leo-xml-inspect-sections-as-list)
       (setq leo-xml-inspect-single-section
             (car (leo--get-result-sections-as-list
                   (leo--get-result-section-list
@@ -37,4 +57,7 @@ The result is also saved to variable leo-xml-inspect-result for playing."
       (setq leo-xml-inspect-single-side-2
             (cadr (leo--get-sides-from-entry
                   leo-xml-inspect-single-entry)))
+      (print xml (current-buffer))
+      (emacs-lisp-mode)
+      (pp-buffer)
       )))
