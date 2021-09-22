c2c76dd1a774a112e247edb484501a3c41ddbcc5
Michael T. Enders <michael@michael-enders.com>
2021-09-22 11:35:40 +0200

    Clarified usage.

--- leo.el
+++ leo.el
@@ -19,7 +19,7 @@
 ;; Usage:
 ;;
 ;; This provides the commands leo-translate-word and
-;; leo-translate-at-point.  Both translate from the language set by the
+;; leo-translate-at-point.  Both translate between the language set by the
 ;; custom variable leo-language to German.
 ;;
 ;; Available languages: en, es, fr, it, ch, pt, ru, pl
